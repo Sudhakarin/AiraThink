b2d83f247e1427d2251e366df9a98003520bad56
Sudhakar Mishra <108603071+Sudhakarin@users.noreply.github.com>
2026-08-20 20:59:18 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,7 +27,7 @@ const config: Config = {
         },
       },
       fontFamily: {
-        display: ["var(--font-sora)", "sans-serif"],
+        display: ["var(--font-poppins)", "sans-serif"],
         body: ["var(--font-inter)", "sans-serif"],
         mono: ["var(--font-jbmono)", "monospace"],
       },
